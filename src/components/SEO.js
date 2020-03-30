99e591cc957ee8c2f0c5ae0a11e54d70bf983629
milan-josipovic <milan.josipovic@protonmail.com>
2020-03-30 06:16:21 +0200

    twitter card try 3

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -37,7 +37,7 @@ const SEO = ({ title, description }) => {
       <meta name="twitter:creator" content={twitterUsername} />
       <meta name="twitter:title" content={siteTitle} />
       <meta name="twitter:description" content={siteDesc} />
-      <meta name="twitter:image" content={`${siteUrl},${image}`} />
+      <meta name="twitter:image" content={`${siteUrl}${image}`} />
     </Helmet>
   )
 }
